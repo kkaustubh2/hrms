01b01c9b0ff302a395617e229f5579403e6904af
kkaustubh2 <723146521q@gmail.com>
2026-07-31 23:51:31 +0530

    Add Department request and response DTOs

---
+++ src/main/java/com/kkaustubh/hrms/department/dto/DepartmentResponse.java
@@ -0,0 +1,15 @@
+package com.kkaustubh.hrms.department.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentResponse {
+    private Long id;
+    private String name;
+}
